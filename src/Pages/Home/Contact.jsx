b14c9b4bc7207d08c5ba90cf15465742bdb5bf74
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-01-25 23:48:31 +0600

    alomost done

--- src/Pages/Home/Contact.jsx
+++ src/Pages/Home/Contact.jsx
@@ -6,17 +6,15 @@ import arrow from '../../Images/arrow.png'
 
 const Contact = () => {
   return <>
-  <Box sx={{height:'46vh',backgroundColor:'#FFFC00',mb:10}}>
-    <Typography display="flex"  sx={{ justifyContent: 'center',pt:7 }} variant='h2'>
-            Need OTP Application?
-    </Typography>
-    <Box display="flex"  sx={{ justifyContent: 'center',ml:-16,mt:3}}>
-        <img style={{width:'25%',marginRight:'25px'}} src={img} />
-        <img style={{height:'5vh',marginTop:'10px'}} src={arrow} />
-        
+    <Box sx={{height:'35vh',backgroundColor:'#FFFC00'}}>
+      <Typography display="flex"  sx={{ justifyContent: 'center',pt:7 }} variant='h2'>
+              Need OTO Applications?
+      </Typography>
+      <Box display="flex"  sx={{ justifyContent: 'center',ml:-16,mt:3}}>
+          <img style={{width:'25%',marginRight:'25px'}} src={img} />
+          <img style={{height:'5vh',marginTop:'10px'}} src={arrow} />
+      </Box>
     </Box>
-    
-  </Box>
   </>;
 };
 
--- src/Pages/Home/World.jsx
+++ src/Pages/Home/World.jsx
@@ -3,101 +3,70 @@ import React from 'react';
 import rectangle from '../../Images/rectangle.png'
 import { Container, Fab, Grid, Typography } from '@mui/material';
 import globe from '../../Images/globe.png';
-import img from '../../Images/image6.png'
-
-
 
 const World = () => {
   return <>
-  {/* <Box classname="wrapper">
-      <img style={{height:'100%'}} src={rectangle} />
-  </Box>
-  <Box classname="wrapper-txt">
-      
-  </Box> */}
-    <div  style={{ backgroundImage: `url(${rectangle})`,backgroundRepeat: 'no-repeat',
-        height:'200vh',width:'100%'}} >
-    <div style={{
+    <Box  style={{ backgroundImage: `url(${rectangle})`,backgroundRepeat: 'no-repeat',
+        height:'150vh',width:'100%'}} >
+      <Box style={{
         position:'relative',
-       top:'70vh'
-          
-}}>
-   <Container>
-   <Grid   container
-  direction="row"
-  justifyContent="center"
-  sx={{ml:5}}
- spacing={2}>
-  <Grid item sx={{color:'white',mt:6}} xs={6} md={5}>
-       <Box display="flex" sx={{flexwrap: 'wrap',flexDirection: 'column'}}>
-       <Box>
-       <Typography variant="h3">
-        The Backbone for 
-        </Typography>
+      top:'70vh'
+      }}>
+        <Container>
+        <Grid   container
+        direction="row"
+        justifyContent="center"
+        sx={{ml:5}}
+      spacing={2}>
+        <Grid item sx={{color:'white',mt:6}} xs={6} md={5}>
+        <Box display="flex" sx={{flexwrap: 'wrap',flexDirection: 'column'}}>
+        <Box>
         <Typography variant="h3">
-         Internet Business 
-        </Typography>
-        <Typography variant="h3">
-         Worldwide
-        </Typography>
-       </Box>
-        <Box sx={{mt:2,color:'#CCCCCC'}}>
-            <Typography>
-            Add di!erent types of vehicles in the 
-            </Typography>
-            <Typography>
-          same app easily. Started with scooters,
-            </Typography>
-            <Typography>
-         but want to add bikes? No problem! 
-            </Typography>
-            <Typography>
-            We’ll help you
-            </Typography>
-
+          The Backbone for 
+          </Typography>
+          <Typography variant="h3">
+          Internet Business 
+          </Typography>
+          <Typography variant="h3">
+          Worldwide
+          </Typography>
+        </Box>
+          <Box sx={{mt:2,color:'#CCCCCC'}}>
+              <Typography>
+                  Add di!erent types of vehicles in the 
+              </Typography>
+              <Typography>
+                same app easily. Started with scooters,
+              </Typography>
+              <Typography>
+                but want to add bikes? No problem! 
+              </Typography>
+              <Typography>
+                We’ll help you
+              </Typography>
             <Fab
-              variant="extended"
-              
-              sx={{
-                backgroundColor: "#FEFB00",
-                "&:hover": {
-                  backgroundColor: "#263238",
-                },
-                px:4,
-                mt: 12,
-              }}
+                variant="extended"
+                sx={{
+                  backgroundColor: "#FEFB00",
+                  "&:hover": {
+                    backgroundColor: "#263238",
+                  },
+                  px:4,
+                  mt: 12,
+                }}
             >
               Get Started
             </Fab>
-        </Box>
-        
-       </Box>
-  </Grid>
-  <Grid item xs={6} md={5}>
-    <img style={{width:'100%'}} src={globe} />
-  </Grid>
-  
-</Grid>
-   </Container>
-      </div>
-      
-      {/* <div style={{
-          
-          position :'relative',
-         
-          
-          zIndex: 2
-      }}>
-          <Box>
-          <img style={{width:'100%'}} src={rectangle} />
           </Box>
-      </div> */}
-      
-    </div>
-      
-    
-  
-
+            </Box>
+            </Grid>
+            <Grid item xs={6} md={5}>
+              <img style={{width:'100%'}} src={globe} />
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
   </>;
 };
 
